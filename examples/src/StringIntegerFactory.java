287334c334d35bc78824414cbc2be6ed7949f60f
Stephen Colebourne <scolebourne@joda.org>
2008-01-01 17:54:28 +0000

    Constructor references

---
+++ examples/src/StringIntegerFactory.java
@@ -0,0 +1,13 @@
+/**
+ * Interface defining a String to Integer factory
+ */
+public interface StringIntegerFactory {
+
+    /**
+     * Creates an Integer from a String.
+     * @param str  the string to parse
+     * @return the resulting integer
+     */
+    Integer create(String str);
+
+}
