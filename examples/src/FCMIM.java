6518dc58d27a0ef9141e35720ba278dbcbfcef4d
Stephen Colebourne <scolebourne@joda.org>
2008-01-21 16:19:58 +0000

    Inner methods part 1

---
+++ examples/src/FCMIM.java
@@ -0,0 +1,36 @@
+
+public class FCMIM {
+
+    private String name;
+
+    public FCMIM(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        try {
+            FCMIM fcm = new FCMIM("Main");
+            fcm.processInnerMethod();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+
+    public void processInnerMethod() throws Exception {
+        StringIntegerFactory factory = #(String str) {
+            System.out.println("IM:" + this);
+            return Integer.parseInt(str);
+        };
+        System.out.println("01:" + factory);
+        System.out.println("02:" + factory.create("7"));
+    }
+
+    public Integer handle(String str) {
+        return 6;
+    }
+    @Override
+    public String toString() {
+        return "FCM-IM:" + name;
+    }
+}
