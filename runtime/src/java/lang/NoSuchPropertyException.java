264179f4890a43bc014f9fe96e15cbec101a6f53
Rémi Forax <forax@users.noreply.github.com>
2008-03-23 21:40:06 +0000

    change serialUID and identation

--- runtime/src/java/lang/NoSuchPropertyException.java
+++ runtime/src/java/lang/NoSuchPropertyException.java
@@ -1,22 +1,21 @@
 package java.lang;
 
 public class NoSuchPropertyException extends Exception {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -1490118501249593881L;
 
-  public NoSuchPropertyException() {
-    super();
-  }
+    public NoSuchPropertyException() {
+        super();
+    }
 
-  public NoSuchPropertyException(String message) {
-    super(message);
-  }
+    public NoSuchPropertyException(String message) {
+        super(message);
+    }
 
-  public NoSuchPropertyException(Throwable cause) {
-    super(cause);
-  }
+    public NoSuchPropertyException(Throwable cause) {
+        super(cause);
+    }
 
-  public NoSuchPropertyException(String message,Throwable cause) {
-    super(message,cause);
-  }
-
+    public NoSuchPropertyException(String message,Throwable cause) {
+        super(message,cause);
+    }
 }
